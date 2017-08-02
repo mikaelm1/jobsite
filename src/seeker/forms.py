c21cd1fd1efeff960c454fdec5b9a5186ddafc4c
Mikael Mukhsikaroyan <mikaelm3981@gmail.com>
2017-08-01 21:12:31 -0700

    Added register form and signal to seeker model

--- src/seeker/forms.py
+++ src/seeker/forms.py
@@ -1,6 +1,27 @@
 from django import forms
 from .models import Seeker, User
 from django.contrib.auth.views import LoginView
+from django.core.exceptions import ValidationError
+
+
+def ForbiddenUsernamesValidator(value):
+    forbidden_usernames = ['admin', 'settings', 'news', 'about', 'help',
+                           'signin', 'signup', 'signout', 'terms', 'privacy',
+                           'cookie', 'new', 'login', 'logout', 'administrator',
+                           'join', 'account', 'username', 'root', 'blog',
+                           'user', 'users', 'billing', 'subscribe', 'reviews',
+                           'review', 'blog', 'blogs', 'edit', 'mail', 'email',
+                           'home', 'job', 'jobs', 'contribute', 'newsletter',
+                           'shop', 'profile', 'register', 'auth',
+                           'authentication', 'campaign', 'config', 'delete',
+                           'remove', 'forum', 'forums', 'download',
+                           'downloads', 'contact', 'blogs', 'feed', 'feeds',
+                           'faq', 'intranet', 'log', 'registration', 'search',
+                           'explore', 'rss', 'support', 'status', 'static',
+                           'media', 'setting', 'css', 'js', 'follow',
+                           'activity', 'questions', 'articles', 'network', ]
+    if value.lower() in forbidden_usernames:
+        raise ValidationError('This is a reserved username.')
 
 
 class SeekerLoginForm(forms.Form):
@@ -12,6 +33,24 @@ class SeekerLoginForm(forms.Form):
         required=True,
     )
 
-    # class Meta:
-    #     model = User
-    #     fields = ('username', 'password')
+
+class SeekerRegisterForm(forms.ModelForm):
+    username = forms.CharField(
+        widget=forms.TextInput(attrs={'class': 'form-control'}),
+        required=True,
+        max_length=30,
+    )
+    password = forms.CharField(
+        widget=forms.PasswordInput(attrs={'class': 'form-control'})
+    )
+    email = forms.CharField(
+        widget=forms.EmailInput(attrs={'class': 'form-control'})
+    )
+
+    class Meta:
+        model = User
+        fields = ('username', 'password', 'email')
+
+    def __init__(self, *args, **kwargs):
+        super(SeekerRegisterForm, self).__init__(*args, **kwargs)
+        self.fields['username'].validators.append(ForbiddenUsernamesValidator)
--- src/seeker/models.py
+++ src/seeker/models.py
@@ -1,5 +1,9 @@
+import logging
 from django.db import models
 from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+
+logger = logging.getLogger(__name__)
 
 
 class Seeker(models.Model):
@@ -8,3 +12,12 @@ class Seeker(models.Model):
 
     def __str__(self):
         return self.user.username
+
+
+def create_seeker(sender, instance, created, **kwargs):
+    if created:
+        seeker = Seeker.objects.get_or_create(user=instance)
+        logger.warning("Seeker: ({})".format(seeker))
+
+
+post_save.connect(create_seeker, sender=User)
