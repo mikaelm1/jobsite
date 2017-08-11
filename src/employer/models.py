e3485e6ecef7b8c76cdb71413a73944f89e1c571
Mikael Mukhsikaroyan <mikaelm3981@gmail.com>
2017-08-11 11:04:07 -0700

    Added job model

--- src/employer/models.py
+++ src/employer/models.py
@@ -3,23 +3,12 @@ from django.contrib.auth.models import User
 
 
 class Employer(models.Model):
-    JOB_TYPES = (
-        ('Full-time', 'Full-time'),
-        ('Part-time', 'Part-time'),
-        ('Internship', 'Internship'),
-        ('Contract', 'Contract'),
-        ('Other', 'Other'),
-    )
     user = models.OneToOneField(User)
     name = models.CharField(max_length=500, blank=False, null=False,
                             unique=True)
     city = models.CharField(max_length=100, blank=False, null=False)
     state = models.CharField(max_length=100, blank=False, null=False)
     web_url = models.CharField(max_length=500, blank=True, null=True)
-    # job_description = models.TextField()
-    # job_title = models.CharField(max_length=500, blank=False, null=False)
-    # job_type = models.CharField(max_length=100, choices=JOB_TYPES,
-                                # default='Other')
 
     class Meta:
         db_table = 'employer'
